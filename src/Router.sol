59b38de0d7aee7fedb9dc63025a71ad9928d08c7
Ary0520 <aryansinhag2006@gmail.com>
2026-03-03 15:05:47 +0530

    added _addliquidity in router

--- src/Router.sol
+++ src/Router.sol
@@ -26,6 +26,8 @@ contract Router{
     error Router__Expired();
     error Router__ZeroAddress();
     error Router__PairNotFound();
+    error Router__SlippageExceeded();
+    error Router__TransferFailed();
 
     constructor(address _factory){
         if(_factory == address(0)){
@@ -45,6 +47,22 @@ contract Router{
     //functions///
     //////////////
 
+    function _safeTransferFrom(address token, address from, address to, uint amount) internal {
+        (bool success, bytes memory data) =
+            token.call(
+                abi.encodeWithSelector(
+                    IERC20.transferFrom.selector,
+                    from,
+                    to,
+                    amount
+                )
+            );
+
+    if (!success || (data.length > 0 && !abi.decode(data, (bool)))) {
+        revert Router__TransferFailed();
+    }
+}
+
     function _quote(uint amountA, uint reserveA, uint reserveB) internal pure returns(uint amountB){
         if(reserveA == 0|| reserveB == 0){
             revert Router__PairNotFound();
@@ -53,6 +71,7 @@ contract Router{
         amountB = (amountA * reserveB)/reserveA;
     }
 
+    //*internal* _addliquidity
     function _addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin) internal returns(uint amountA, uint amountB){
 
         address pair = IFactory(factory).getPair(tokenA, tokenB);
@@ -61,13 +80,59 @@ contract Router{
         }
         pair = IFactory(factory).getPair(tokenA, tokenB); //fetch again
 
+        //fetch reserves->
         (uint reserve0, uint reserve1, ) = IPair(pair).getReserves();
+
         //pair stores sorted tokens, so arrange reserves0/1 according to that->
         address token0 = IPair(pair).token0();
-        address token1 = IPair(pair).token1();
+        // address token1 = IPair(pair).token1();
+
+        uint reserveA;
+        uint reserveB;
+
         if(tokenA == token0){
-            
+            reserveA = reserve0;
+            reserveB = reserve1;
+        }else{
+            reserveA = reserve1;
+            reserveB = reserve0;
         }
+
+        //if pool= empty->
+        if(reserveA == 0 && reserveB == 0){
+            amountA = amountADesired;
+            amountB = amountBDesired;
+        }else{
+            uint amountBOptimal = _quote(amountADesired, reserveA, reserveB);
+            if(amountBOptimal <= amountBDesired){
+                amountA = amountADesired;
+                amountB = amountBOptimal;
+            }else{
+                uint amountAoptimal= _quote(amountBDesired, reserveB, reserveA);
+                amountA = amountAoptimal;
+                amountB = amountBDesired;
+            }
+        }
+
+        //slippage protection->
+        if(amountA < amountAMin || amountB < amountBMin ){
+            revert Router__SlippageExceeded();
+        }
+
+    }
+
+    //external addliquidity(callable by user)->
+    function addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin, address to, uint deadline) external ensure(deadline) returns(uint amountA, uint amountB, uint liquidity){
+
+        (amountA, amountB) = _addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+        address pair = IFactory(factory).getPair(tokenA, tokenB);
+
+        _safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        _safeTransferFrom(tokenB, msg.sender, pair, amountB);
+
+        liquidity = IPair(pair).mint(to);
+        return(amountA, amountB, liquidity);
+
     }
 
 }
